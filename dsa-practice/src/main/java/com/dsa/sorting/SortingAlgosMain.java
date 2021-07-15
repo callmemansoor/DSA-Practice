c627030964a45f26c3aecaae8fc6fa5a2dd905fc
mansoor7866 <pslv786@gmail.com>
2021-07-15 22:03:34 +0530

    dynamic programming fibonacci sum

--- dsa-practice/src/main/java/com/dsa/sorting/SortingAlgosMain.java
+++ dsa-practice/src/main/java/com/dsa/sorting/SortingAlgosMain.java
@@ -5,7 +5,7 @@ public class SortingAlgosMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//		int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		int[] sortArr = new int[] { 56, 10, 25, 63, 14, 98, 110, 26, 30, 1, 6 };
 
 		int[] selectionSortedArray = new SelectionSort().sort(sortArr);
